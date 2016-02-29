b06c712cd7af44a7c8943289ec507fa09e1ac074
Mike Labita <mlabita37@gmail.com>
2016-02-29 14:26:17 -0500

    Separates kick functions into it's own kick.js file

---
+++ public/js/kick.js
@@ -0,0 +1,73 @@
+function playKick(kick, startTime, quarterNoteTime){
+
+  kick = kick;
+  startTime = startTime;
+  quarterNoteTime = quarterNoteTime;
+
+  var kick1 = $('#kick1').val(),
+      kick2 = $('#kick2').val(),
+      kick3 = $('#kick3').val(),
+      kick4 = $('#kick4').val(),
+      kick5 = $('#kick5').val(),
+      kick6 = $('#kick6').val(),
+      kick7 = $('#kick7').val(),
+      kick8 = $('#kick8').val(),
+      kick9 = $('#kick9').val(),
+      kick10 = $('#kick10').val(),
+      kick11 = $('#kick11').val(),
+      kick12 = $('#kick12').val(),
+      kick13 = $('#kick13').val(),
+      kick14 = $('#kick14').val(),
+      kick15 = $('#kick15').val(),
+      kick16 = $('#kick16').val();
+
+    if (sixteenths == true){
+    var kickArray = [
+      kick1, kick2, kick3, kick4, kick5, kick6, kick7, kick8,
+      kick9, kick10, kick11, kick12, kick13, kick14, kick15, kick16,
+      kick1, kick2, kick3, kick4, kick5, kick6, kick7, kick8,
+      kick9, kick10, kick11, kick12, kick13, kick14, kick15, kick16,
+      kick1, kick2, kick3, kick4, kick5, kick6, kick7, kick8,
+      kick9, kick10, kick11, kick12, kick13, kick14, kick15, kick16,
+      kick1, kick2, kick3, kick4, kick5, kick6, kick7, kick8,
+      kick9, kick10, kick11, kick12, kick13, kick14, kick15, kick16
+   ];
+
+    for (var i=0; i < kickArray.length * 2; i++){
+      if (kickArray[i] == 1 && kickArray[i].id == "kick1"){
+        playSound(kick, startTime);
+      }else if (kickArray[i] == 1){
+        playSound(kick, startTime + i*0.25*quarterNoteTime);
+        flashLights('kick'+(i+1));
+      }
+    };
+    }else if(eighths == true){
+      var kickArray = [
+        kick1, kick2, kick3, kick4, kick5, kick6, kick7, kick8,
+        kick9, kick10, kick11, kick12, kick13, kick14, kick15, kick16,
+        kick1, kick2, kick3, kick4, kick5, kick6, kick7, kick8,
+        kick9, kick10, kick11, kick12, kick13, kick14, kick15, kick16
+      ];
+      for (var i=0; i < kickArray.length; i++){
+        if (kickArray[i] == 1 && kickArray[i].id == "kick1"){
+          playSound(kick, startTime);
+        }else if (kickArray[i] == 1){
+          playSound(kick, startTime + i*0.5*quarterNoteTime);
+          flashLights('kick'+(i+1));
+        }
+      };
+    }else{
+      var kickArray = [
+        kick1, kick2, kick3, kick4, kick5, kick6, kick7, kick8,
+        kick9, kick10, kick11, kick12, kick13, kick14, kick15, kick16
+      ];
+      for (var i=0; i < kickArray.length; i++){
+        if (kickArray[i] == 1 && kickArray[i].id == "kick1"){
+          playSound(kick, startTime);
+        }else if (kickArray[i] == 1){
+          playSound(kick, startTime + i*quarterNoteTime);
+          flashLights('kick'+(i+1));
+        }
+      };
+    }
+  };
--- public/js/rhythm.js
+++ public/js/rhythm.js
@@ -56,7 +56,7 @@ function stopSound() {
   source.stop(context.currentTime);
 }
 
-// Plays Loop 1
+// Play Loop
 function myLoop(bufferList) {
     var kick = bufferList[0];
     var snare = bufferList[1];
@@ -65,23 +65,7 @@ function myLoop(bufferList) {
     var tamb = bufferList[4];
     var cymbal = bufferList[5];
 
-    var kick1 = $('#kick1').val(),
-        kick2 = $('#kick2').val(),
-        kick3 = $('#kick3').val(),
-        kick4 = $('#kick4').val(),
-        kick5 = $('#kick5').val(),
-        kick6 = $('#kick6').val(),
-        kick7 = $('#kick7').val(),
-        kick8 = $('#kick8').val(),
-        kick9 = $('#kick9').val(),
-        kick10 = $('#kick10').val(),
-        kick11 = $('#kick11').val(),
-        kick12 = $('#kick12').val(),
-        kick13 = $('#kick13').val(),
-        kick14 = $('#kick14').val(),
-        kick15 = $('#kick15').val(),
-        kick16 = $('#kick16').val(),
-        snare1 = $('#snare1').val(),
+    var snare1 = $('#snare1').val(),
         snare2 = $('#snare2').val(),
         snare3 = $('#snare3').val(),
         snare4 = $('#snare4').val(),
@@ -172,57 +156,7 @@ if (play == true){
     console.log(tempo);
     var quarterNoteTime = 60 / tempo;
 
-    // KICK
-    if (sixteenths == true){
-    var kickArray = [
-      kick1, kick2, kick3, kick4, kick5, kick6, kick7, kick8,
-      kick9, kick10, kick11, kick12, kick13, kick14, kick15, kick16,
-      kick1, kick2, kick3, kick4, kick5, kick6, kick7, kick8,
-      kick9, kick10, kick11, kick12, kick13, kick14, kick15, kick16,
-      kick1, kick2, kick3, kick4, kick5, kick6, kick7, kick8,
-      kick9, kick10, kick11, kick12, kick13, kick14, kick15, kick16,
-      kick1, kick2, kick3, kick4, kick5, kick6, kick7, kick8,
-      kick9, kick10, kick11, kick12, kick13, kick14, kick15, kick16
-   ];
-
-    for (var i=0; i < kickArray.length * 2; i++){
-      if (kickArray[i] == 1 && kickArray[i].id == "kick1"){
-        playSound(kick, startTime);
-      }else if (kickArray[i] == 1){
-        playSound(kick, startTime + i*0.25*quarterNoteTime);
-        flashLights('kick'+(i+1));
-      }
-    };
-    }else if(eighths == true){
-      var kickArray = [
-        kick1, kick2, kick3, kick4, kick5, kick6, kick7, kick8,
-        kick9, kick10, kick11, kick12, kick13, kick14, kick15, kick16,
-        kick1, kick2, kick3, kick4, kick5, kick6, kick7, kick8,
-        kick9, kick10, kick11, kick12, kick13, kick14, kick15, kick16
-      ];
-      for (var i=0; i < kickArray.length; i++){
-        if (kickArray[i] == 1 && kickArray[i].id == "kick1"){
-          playSound(kick, startTime);
-        }else if (kickArray[i] == 1){
-          playSound(kick, startTime + i*0.5*quarterNoteTime);
-          flashLights('kick'+(i+1));
-        }
-      };
-    }else{
-      var kickArray = [
-        kick1, kick2, kick3, kick4, kick5, kick6, kick7, kick8,
-        kick9, kick10, kick11, kick12, kick13, kick14, kick15, kick16
-      ];
-      for (var i=0; i < kickArray.length; i++){
-        if (kickArray[i] == 1 && kickArray[i].id == "kick1"){
-          playSound(kick, startTime);
-        }else if (kickArray[i] == 1){
-          playSound(kick, startTime + i*quarterNoteTime);
-          flashLights('kick'+(i+1));
-        }
-      };
-    }
-
+    playKick(kick, startTime, quarterNoteTime);
     // SNARE
     if (sixteenths == true){
     var snareArray = [
